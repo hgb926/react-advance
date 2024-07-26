c50cbdc532b44c4bbb4602d2e7d1ebde17016b19
hgb926 <hgb926@naver.com>
2024-07-26 10:58:54 +0900

    0726 새로운 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
 import ReduxCounter from './redux-practice/components/ReduxCounter';
+import Header from './redux-practice/components/Header'
+import Auth from './redux-practice/components/Auth'
+import UserProfile from './redux-practice/components/UserProfile'
+
 
 const App = () => {
 
 
     return (
         <>
+            <Header />
+            <Auth />
+            {/*<UserProfile />*/}
             <ReduxCounter />
         </>
     );
