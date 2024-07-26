817d8d4c488b72aad6a6a0b26d6ea72380dded6a
hgb926 <hgb926@naver.com>
2024-07-26 09:57:35 +0900

    0726 리듀서의 기본적인 흐름

--- src/redux-practice/components/ReduxCounter.js
+++ src/redux-practice/components/ReduxCounter.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styles from './ReduxCounter.module.css';
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import { INCREMENT, DECREMENT } from "../store/index"
 
 const ReduxCounter = () => {
 
     // useSelector라는 훅을 통해 redux store에 있는 상태값을 가져옴
-    const counter = useSelector(state => state.counter);
-    console.log(counter)
+    const counter = useSelector(state => state.counter); // 변수
+
+    // 리덕스 스토어에 상태값 변경을 위해 액션을 호출하는 훅
+    const dispatch = useDispatch(); // setter
+
+
+    const increaseHandler = e => {
+      // redux store에 접근해서 상태값을 변경시켜야 함
+      // 리덕스에서는 상태값 변경을 위해 액션함수를 호출해야 함.
+      // 액션 함수는 리덕스 스토어 내부에 있는 리듀서가 가지고 있음.
+
+      // dispatch에는 인자로 어떤 변경을 할지 type과 변경에 필요한
+      // payload를 전송 (없으면 안줘도 됨)
+      dispatch({ type: INCREMENT });
+    };
+
+    const decreaseHandler = e => {
+        dispatch({ type: DECREMENT })
+    };
 
     return (
         <main className={styles.counter}>
@@ -14,8 +32,8 @@ const ReduxCounter = () => {
             <div className={styles.value}>{counter}</div>
 
             <div>
-                <button>Increment</button>
-                <button>Decrement</button>
+                <button onClick={increaseHandler}>Increment</button>
+                <button onClick={decreaseHandler}>Decrement</button>
             </div>
 
             <button>Toggle Counter</button>
