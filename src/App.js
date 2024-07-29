118350e12c6effa058e206cbd3bdddb52ee68ddb
hgb926 <hgb926@naver.com>
2024-07-29 09:46:38 +0900

    0729 카트 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, {useState} from 'react';
 import Layout from './redux-cart/components/Layout/Layout'
 import Cart from './redux-cart/components/Cart/Cart'
 import Products from './redux-cart/components/Shop/Products'
+import { useSelector, useDispatch } from 'react-redux'
 
 
 const App = () => {
 
+    const isVisible = useSelector(state => state.ui.cartIsVisible);
+    console.log(isVisible)
+
 
     return (
         <Layout>
-            <Cart/>
+            {isVisible && <Cart/>}
             <Products/>
         </Layout>
     );
